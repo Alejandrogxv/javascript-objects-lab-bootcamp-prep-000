36800c495b970c62303b13469929fecfd0a593ec
Alejandro Gonzalez <alejandrogxv@gmail.com>
2018-08-14 15:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},obj,{[key]:value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
